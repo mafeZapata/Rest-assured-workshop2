5c93e21859ddd18edcc5066eeb4e3b1a0629746a
mafeZapata <mafez0427@gmail.com>
2025-04-03 16:04:33 -0500

    second commit
    updates and changes into the configurations

---
+++ restAssured2/src/test/java/workshop/GetAndPostExample.java
@@ -0,0 +1,62 @@
+package workshop;
+
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+import io.restassured.*;
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class GetAndPostExample {
+
+    @Test
+    public void testGet() {
+        System.out.println("Running test");
+        RestAssured.baseURI = "https://reqres.in/api";
+
+        when().
+                get("/users?page=2").
+                then().
+                statusCode(200).
+                body("data.size()", is(6)).
+                body("data.first_name", hasItems("George", "Rachel"));
+
+    };
+
+    @Test
+    public void testPost() {
+
+        JSONObject request = new JSONObject();
+
+        request.put("name", "Ernesto Perez");
+        request.put("job", "QA Automation");
+
+        RestAssured.baseURI =  "https://reqres.in/api";
+
+        given().
+                header("Content-Type", "application/json").
+                contentType(ContentType.JSON).
+                body(request.toJSONString()).
+                when().
+                post("/users").
+                then().
+                statusCode(201).
+                log().all();
+        System.out.println(request.toJSONString());
+    }
+    //Ejercicio pokeapi Get
+     @Test
+     public void testGetPokemon() {
+         RestAssured.baseURI = "https://pokeapi.co/api/v2";
+         String pokemonName = "";
+         Response response =
+                 when().
+                         get("/pokemon/" + pokemonName).
+                         then().
+                         statusCode(200).
+                         extract().response();
+         System.out.println(response.asPrettyString());
+     }
+
+
+}
---
+++ restAssured2/src/test/java/workshop/PutPatchDeleteExample.java
@@ -0,0 +1,69 @@
+package workshop;
+
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+import io.restassured.*;
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class PutPatchDeleteExample {
+
+    @Test
+    public void testPut() {
+
+        JSONObject request = new JSONObject();
+
+        request.put("name", "Ernesto Perez");
+        request.put("job", "QA Automation");
+
+        RestAssured.baseURI =  "https://reqres.in/api";
+
+        given().
+                header("Content-Type", "application/json").
+                contentType(ContentType.JSON).
+                body(request.toJSONString()).
+                when().
+                put("/users/2").
+                then().
+                statusCode(200).
+                log().all();
+
+    }
+
+    @Test
+    public void testPatch() {
+
+        JSONObject request = new JSONObject();
+
+        request.put("name", "Ernesto Perez");
+        request.put("job", "QA Automation");
+
+        RestAssured.baseURI =  "https://reqres.in";
+
+        given().
+                header("Content-Type", "application/json").
+                contentType(ContentType.JSON).
+                body(request.toJSONString()).
+                when().
+                patch("/api/users/2").
+                then().
+                statusCode(200).
+                log().all();
+
+    }
+    @Test
+    public void testDelete() {
+
+        RestAssured.baseURI =  "https://reqres.in";
+
+        when().
+                delete("/api/users/2").
+                then().
+                statusCode(204).
+                log().all();
+
+    }
+
+
+}
